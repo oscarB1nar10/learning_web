a159deed601adc574044659b9ee7dbb46cd406da
Oscar <oscivaramtin@gmail.com>
2022-10-20 06:41:10 -0500

    w/Python_and_Django
    Python basis and Django basis

---
+++ python/list.py
@@ -0,0 +1,11 @@
+
+from unicodedata import name
+
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names[0])
+
+names.append("Draco")
+
+print(names)
---
+++ python/sequences.py
@@ -0,0 +1,3 @@
+name ="Oscar"
+
+print(name[0])
